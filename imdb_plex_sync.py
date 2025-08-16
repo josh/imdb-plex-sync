095c0925bbae13b70634a09899b40a448041a751
Joshua Peek <josh@users.noreply.github.com>
2025-08-15 17:11:05 -0700

    ssort

--- imdb_plex_sync.py
+++ imdb_plex_sync.py
@@ -15,67 +15,6 @@ from plexapi.video import Video  # type: ignore
 logger = logging.getLogger("imdb-trakt-sync")
 
 
-@click.command()
-@click.option(
-    "--imdb-watchlist-url",
-    required=True,
-    envvar="IMDB_WATCHLIST_URL",
-)
-@click.option(
-    "--plex-username",
-    envvar="PLEX_USERNAME",
-)
-@click.option(
-    "--plex-password",
-    envvar="PLEX_PASSWORD",
-)
-@click.option(
-    "--plex-token",
-    envvar="PLEX_TOKEN",
-)
-@click.option(
-    "--verbose",
-    "-v",
-    is_flag=True,
-    help="Enable verbose logging",
-    envvar="ACTIONS_RUNNER_DEBUG",
-)
-def main(
-    imdb_watchlist_url: str,
-    plex_username: str | None,
-    plex_password: str | None,
-    plex_token: str | None,
-    verbose: bool,
-) -> None:
-    logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO)
-
-    imdb_ids = _fetch_imdb_watchlist(imdb_watchlist_url)
-    imdb_keys = set(_imdb_to_plex_ids(imdb_ids))
-
-    account = MyPlexAccount(
-        username=plex_username,
-        password=plex_password,
-        token=plex_token,
-    )
-    plex_keys: set[str] = set(
-        item.key.replace("/library/metadata/", "") for item in account.watchlist()
-    )
-
-    for key in imdb_keys - plex_keys:
-        video = _find_by_plex_guid(account, key)
-        logger.info("+ %s", video.title)
-        video.addToWatchlist()
-
-    for key in plex_keys - imdb_keys:
-        video = _find_by_plex_guid(account, key)
-        logger.info("- %s", video.title)
-        video.removeFromWatchlist()
-
-
-def _fetch_imdb_watchlist(url: str) -> list[str]:
-    return [row["Const"] for row in csv.DictReader(_iterlines(url))]
-
-
 def _iterlines(path: Path | str) -> Iterator[str]:
     if isinstance(path, str) and path.startswith("http"):
         logger.debug("Fetching remote '%s'", path)
@@ -86,6 +25,10 @@ def _iterlines(path: Path | str) -> Iterator[str]:
         logger.debug("Reading local file '%s'", path)
         with open(path) as f:
             yield from f
+
+
+def _fetch_imdb_watchlist(url: str) -> list[str]:
+    return [row["Const"] for row in csv.DictReader(_iterlines(url))]
 
 
 def _sparql(query: str) -> Any:
@@ -141,6 +84,63 @@ def _find_by_plex_guid(account: MyPlexAccount, ratingkey: str) -> Video:
     return account.fetchItem(
         f"https://metadata.provider.plex.tv/library/metadata/{ratingkey}"
     )
+
+
+@click.command()
+@click.option(
+    "--imdb-watchlist-url",
+    required=True,
+    envvar="IMDB_WATCHLIST_URL",
+)
+@click.option(
+    "--plex-username",
+    envvar="PLEX_USERNAME",
+)
+@click.option(
+    "--plex-password",
+    envvar="PLEX_PASSWORD",
+)
+@click.option(
+    "--plex-token",
+    envvar="PLEX_TOKEN",
+)
+@click.option(
+    "--verbose",
+    "-v",
+    is_flag=True,
+    help="Enable verbose logging",
+    envvar="ACTIONS_RUNNER_DEBUG",
+)
+def main(
+    imdb_watchlist_url: str,
+    plex_username: str | None,
+    plex_password: str | None,
+    plex_token: str | None,
+    verbose: bool,
+) -> None:
+    logging.basicConfig(level=logging.DEBUG if verbose else logging.INFO)
+
+    imdb_ids = _fetch_imdb_watchlist(imdb_watchlist_url)
+    imdb_keys = set(_imdb_to_plex_ids(imdb_ids))
+
+    account = MyPlexAccount(
+        username=plex_username,
+        password=plex_password,
+        token=plex_token,
+    )
+    plex_keys: set[str] = set(
+        item.key.replace("/library/metadata/", "") for item in account.watchlist()
+    )
+
+    for key in imdb_keys - plex_keys:
+        video = _find_by_plex_guid(account, key)
+        logger.info("+ %s", video.title)
+        video.addToWatchlist()
+
+    for key in plex_keys - imdb_keys:
+        video = _find_by_plex_guid(account, key)
+        logger.info("- %s", video.title)
+        video.removeFromWatchlist()
 
 
 if __name__ == "__main__":
